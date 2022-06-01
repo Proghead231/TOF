9ee21ad63931621ed60fb49e483b33eccca54882
Proghead231 <67262626+Proghead231@users.noreply.github.com>
2022-06-01 22:33:07 +0545

    Update folium_map2.py

--- folium_map2.py
+++ folium_map2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import plotly.express as px
+#import plotly.express as px
 import geopandas as gpd
 import os
 import pyproj
